6e9b60dddb6661e29e63eba7b8a96d8f45b22fe9
Ezjq <kokieunz@gmail.com>
2025-01-06 22:05:00 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,3 +1,3 @@
 getgenv().Games = {
-  [100945292362349] = "https://raw.githubusercontent.com/Ezjpq/NaruHub/refs/heads/main/Bloxfruitsbutverygood.lua", -- Blox Fruits But Very Good
+  [100945292362349] = "https://raw.githubusercontent.com/Ezjpq/NaruHub/refs/heads/main/Bloxfruitsbutverygood.lua" -- Blox Fruits But Very Good
 }
