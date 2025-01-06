87d6f523e2d762a5c1293c2a6cb3585615b73054
Ezjq <kokieunz@gmail.com>
2025-01-06 22:03:35 +0800

    Update NaruHub.lua

--- NaruHub.lua
+++ NaruHub.lua
@@ -1,14 +1,6 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjpq/NaruHub/refs/heads/main/GameList.lua"))()
-
-local isSupported = false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjpq/NaruHub/refs/heads/main/GameList.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
-        isSupported = true
-        break
     end
-end
-
-if not isSupported then
-    game.Players.LocalPlayer:Kick("Game Not Supported!")
 end
